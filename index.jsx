5e6378057a41793aca25cfbb1409cc86291c0c89
JorgeHerter <159959563+JorgeHerter@users.noreply.github.com>
2024-07-08 20:26:44 -0600

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MainView from './MainView.jsx';
+// Import statement to indicate that you need to bundle `./index.scss`
+import "./index.scss";
+
+// Main component (will eventually use all the others)
+const MyFlixApplication = () => {
+  return (
+    <div className="my-flix">
+      <MainView />
+    </div>
+  );
+};
+
+// Finds the root of your app
+const container = document.querySelector("#root");
+const root = createRoot(container);
+
+// Tells React to render your app in the root DOM element
+root.render(<MyFlixApplication />);
